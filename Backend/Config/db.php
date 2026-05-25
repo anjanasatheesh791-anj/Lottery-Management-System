597dd44f2258d7afeb1685bb3d705402cbbf4816
Anjana <anjanasatheesh791@gmail.com>
2026-05-25 21:07:54 +0530

    Update db.php

--- Backend/Config/db.php
+++ Backend/Config/db.php
@@ -1,15 +1,16 @@
 <?php
 
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$password = getenv("DB_PASS");
-$dbname = getenv("DB_NAME");
-$port = getenv("DB_PORT");
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$dbname = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("DB Connection failed: " . $conn->connect_error);
-}
+echo json_encode([
+    "host" => $host,
+    "user" => $user,
+    "dbname" => $dbname,
+    "port" => $port
+]);
 
 ?>
