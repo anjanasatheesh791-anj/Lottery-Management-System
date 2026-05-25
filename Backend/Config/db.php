206c614c1ad1efd3528e741b2c63a1ad893f82ae
Anjana <anjanasatheesh791@gmail.com>
2026-05-25 21:12:05 +0530

    Testing

--- Backend/Config/db.php
+++ Backend/Config/db.php
@@ -1,16 +1,11 @@
 <?php
 
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$password = getenv("MYSQLPASSWORD");
-$dbname = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
-
 echo json_encode([
-    "host" => $host,
-    "user" => $user,
-    "dbname" => $dbname,
-    "port" => $port
+    "host" => getenv("MYSQLHOST"),
+    "user" => getenv("MYSQLUSER"),
+    "password" => getenv("MYSQLPASSWORD"),
+    "database" => getenv("MYSQLDATABASE"),
+    "port" => getenv("MYSQLPORT")
 ]);
 
-?>
+exit;
